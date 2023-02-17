0610678144c607e650d9d53b5673258f3c061bbb
Elliott Thacker <ElliottThacker85@gmail.com>
2023-02-17 08:56:55 -0800

    added Nav Layout

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,6 @@
 import '../styles/globals.css'
 import { Inter } from '@next/font/google'
+import GlobalNav from '@/ui/Global.Nav'
 
 const inter = Inter({
   subsets: ['latin'],
@@ -15,7 +16,9 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
     <html lang="en" className={`${inter.variable} font-sans`}>
       <head />
 
-      <body>{children}</body>
+      <body>
+        <GlobalNav/>
+      </body>
     </html>
   )
 }
--- app/page.tsx
+++ app/page.tsx
@@ -1,16 +1,13 @@
 import React from 'react'
-import Header from './Header'
 
 function page() {
   return (
     <div>
-      <Header />
       <div className="">
-        <div
-          className="items-center flex flex-col justify-center text-9xl mt-10">
+        <div className="items-center flex flex-col justify-center text-9xl mt-10">
           <h1 className="text-primary-500 ">Home Page</h1>
         </div>
-      </div> 
+      </div>
     </div>
   )
 }
