4bc22c6fab9efb72c9de98c26bea44b7915ef192
Elliott Thacker <ElliottThacker85@gmail.com>
2023-03-03 08:31:12 -0800

    add generateStaticParams to rpc function returns paths and static generated paths to optimaize proformance

--- app/rpc/[categoriesSlug]/[subcategoriesSlug]/[productSlug]/page.tsx
+++ app/rpc/[categoriesSlug]/[subcategoriesSlug]/[productSlug]/page.tsx
@@ -1,11 +1,20 @@
 import supabase from '@/lib/supabase'
 import { notFound } from 'next/navigation'
 
+export async function generateStaticParams() {
+  const { data: slugs } = await supabase.rpc('get_product_info')
+
+  return slugs?.map((slug) => ({
+    categoriesSlug: slug.categoriesslug,
+    subcategoriesSlug: slug.subcategoriesslug,
+    productSlug: slug.productslug,
+  })) ?? []
+}
+
 export default async function Post({ params: { productSlug } }: { params: { productSlug: string } }) {
-  const { data } = await supabase.from('products').select().match({productSlug}).single()
+  const { data } = await supabase.from('products').select().match({ productSlug }).single()
   if (!data) {
     notFound()
   }
   return <pre>{JSON.stringify(data, null, 2)}</pre>
 }
-
